5496692813a22698a0644577e18322bb7d6d43ec
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2024-10-31 13:47:45 +0300

    Update auth.js

--- backend/auth.js
+++ backend/auth.js
@@ -1,9 +1,9 @@
 const mongoose = require("mongoose");
 
 const userSchema = new mongoose.Schema({
-    name:{type:String,required:true},
-    email:{type:String,required:true,unique:true},
-    password:{type:String,required:true},
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true }, // `unique: true` creates an index
+  password: { type: String, required: true }
 });
 
 module.exports = mongoose.model("UserNames",userSchema);
