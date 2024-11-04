8b83d2501e5a026c72db8edff4242eb08d8905d6
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2024-11-04 14:33:38 +0300

    Update schema.js

--- backend/schema.js
+++ backend/schema.js
@@ -1,13 +1,12 @@
 const mongoose = require("mongoose");
 
-const dataSchema = new mongoose.Schema({
-   date: { type: String, required: true },  // Changed to String to store formatted date
-   name: { type: String, required: true },
-   quantity: { type: Number, required: true },
-   price: { type: Number, required: true },
-   total: { type: Number, required: true },
-   sale: { type: String, required: true }
-   
+const salesSchema = new mongoose.Schema({
+  date: { type: Date, required: true },
+  name: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  price: { type: Number, required: true },
+  total: { type: Number, required: true },
+  sale: { type: Number, required: true }
 });
 
-module.exports = mongoose.model("SalesData", dataSchema);
+module.exports = mongoose.model("Sales", salesSchema);
