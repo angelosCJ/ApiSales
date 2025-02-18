db7ad1b2660c9f31e89cf4b04acc7f76179fe7c6
angelosCJ <83297899+angelosCJ@users.noreply.github.com>
2025-02-18 16:22:18 +0300

    Create store.js

---
+++ store.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const storage = new mongoose.Schema({
+    ItemName:{type:String,required:true},
+    Cartons:{type:String,required:true},
+    QuantityNumber:{type:String,required:true},
+    Rprice:{type:String,required:true},
+    CWprice:{type:String,required:true},
+    CRprice:{type:String,required:true},
+    StockPrice:{type:String,required:true},
+    StockProfit:{type:String,required:true},
+});
+
+module.exports = mongoose.model("stokestorage",storage);
